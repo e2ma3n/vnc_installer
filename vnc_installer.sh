63fdf9858084ea71d4f9ed0bb055e6bc8eab2d2f
Iman Homayouni <e2ma3n@gmail.com>
2021-03-11 22:12:16 +0330

    Update vnc_installer.sh

--- vnc_installer.sh
+++ vnc_installer.sh
@@ -117,7 +117,6 @@ elif [ "$q" = "2" ] ; then
     echo '#!/bin/bash' > ~/.vnc/xstartup
     echo 'xrdb $HOME/.Xresources' >> ~/.vnc/xstartup
     echo 'startxfce4 &' >> ~/.vnc/xstartup
-    
 
 fi
 # ------------------------------------------------------------------------------------------------------- #
