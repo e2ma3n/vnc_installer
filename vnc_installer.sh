c35a685cd05410bb70f8365a62c3cdfb6a4c11ca
Iman Homayouni <e2ma3n@gmail.com>
2021-03-11 22:01:45 +0330

    Update vnc_installer.sh

--- vnc_installer.sh
+++ vnc_installer.sh
@@ -1,66 +1,178 @@
 #! /bin/bash
+# Programming and idea by : Iman Homayouni
+# Gitbub : https://github.com/iman-homayouni
+# Email : homayouni.iman@Gmail.com
+# Website : http://www.homayouni.info
+# License : GPL v2.0
+# Last update : 11-March-2021_19:53:05
+# vnc_installer v1.0.1
+# ------------------------------------------------------------------------------------------------------- #
+# SUCCESSFULLY TESTED IN UBUNTU 16.04 [XENIAL]
+# SUCCESSFULLY TESTED IN UBUNTU 18.04 [BIONIC]
+# SUCCESSFULLY TESTED IN UBUNTU 20.04 [FOCAL]
+# SUCCESSFULLY TESTED IN DEBIAN 10.X [BUSTER]
+# ------------------------------------------------------------------------------------------------------- #
 
+
+# CHECK USER PRIVILEGE # -------------------------------------------------------------------------------- #
 if [ "$UID" != "0" ] ; then
-    echo "[!] use root user"
+    echo "[!] USE ROOT USER"
     exit 1
 fi
+# ------------------------------------------------------------------------------------------------------- #
 
-# Step 01 # ----------------- #
+
+# UPDATE AND UPGRADE SYSTEM # --------------------------------------------------------------------------- #
 apt-get -y update
 apt-get -y dist-upgrade
-# --------------------------- #
+apt -y autoremove
+apt-get -y -f install
+# ------------------------------------------------------------------------------------------------------- #
 
-sleep 3
-clear
 
-# Step 02 # ------------------------ #
-apt install xfce4 xfce4-goodies -y
-apt install tightvncserver -y
-apt install firefox terminator -y
-apt install xfonts-base -y
-# ---------------------------------- #
+# SELECT DESKTOP ENVIRONMENT # -------------------------------------------------------------------------- #
+echo -e "[>] --------------------------------------------------------------------------------- [<]"
+echo -e "[1] INSTALL MATE DESKTOP"
+echo -e "[2] INSTALL XFCE DESKTOP"
+echo -en "[?] WHICH ONE ? [1/2] : " ; read q
 
-sleep 3
-clear
+if [ "$q" = "1" ] ; then
+    which lsb_release &> /dev/null
+    if [ "$?" = "0" ] ; then
+        if [ "$(lsb_release -cs)" = "focal" ] ; then
+            echo -e "[>] CAN NOT INSTALL MATE DESKTOP ON FOCAL"
+            exit 1
+        fi
+    fi
+    echo -e "[>] --------------------------------------------------------------------------------- [<]"
+    apt-get install -y mate-desktop-environment-extra
+elif [ "$q" = "2" ] ; then
+    echo -e "[>] --------------------------------------------------------------------------------- [<]"
+    apt-get install -y xfce4 xfce4-goodies
+    apt-get install -y xfonts-base
+else
+    echo -e "[>] BAD OPTION"
+    echo -e "[>] --------------------------------------------------------------------------------- [<]"
+    exit 1
+fi
+# ------------------------------------------------------------------------------------------------------- #
 
-# Step 03 # ------------------------------------------------------------------------------- #
+
+# INSTALL NECESSARY PACKAGES # -------------------------------------------------------------------------- #
+apt-get install -y net-tools
+apt-get install -y tightvncserver
+apt-get install -y firefox
+apt-get install -y firefox-esr
+apt-get install -y terminator
+# ------------------------------------------------------------------------------------------------------- #
+
+
+# RUN vncserver COMMAND # ------------------------------------------------------------------------------- #
 for (( ;; )) ; do
     vncserver
     if [ "$?" = "0" ] ; then
         break
     fi
 done
+# ------------------------------------------------------------------------------------------------------- #
 
+
+# CLOSE OTHER VNC SESSIONS # ---------------------------------------------------------------------------- #
 for (( i=1 ; i <= 10 ; i++ )) ; do
     vncserver -kill :$i &> /dev/null
 done
+# ------------------------------------------------------------------------------------------------------- #
 
+
+# CREATE vnc.kill SCRIPT # ------------------------------------------------------------------------------ #
 echo 'for (( i=1 ; i <= 10 ; i++ )) ; do' > /root/vnc.kill
 echo '    vncserver -kill :$i &> /dev/null' >> /root/vnc.kill
 echo 'done' >> /root/vnc.kill
+# ------------------------------------------------------------------------------------------------------- #
 
+
+# CHANGE vnc.kill SCRIPT PERMISSIONS # ------------------------------------------------------------------ #
 chmod +x /root/vnc.kill
-mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+# ------------------------------------------------------------------------------------------------------- #
 
-echo '#!/bin/bash' > ~/.vnc/xstartup
-echo 'xrdb $HOME/.Xresources' >> ~/.vnc/xstartup
-echo 'startxfce4 &' >> ~/.vnc/xstartup
+
+# CREATE xstartup FILE # -------------------------------------------------------------------------------- #
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak &> /dev/null
+
+if [ "$q" = "1" ] ; then
+
+    echo '#!/bin/bash' > ~/.vnc/xstartup
+    echo -e 'unset DBUS_SESSION_BUS_ADDRESS' >> ~/.vnc/xstartup
+    echo -e '[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup' >> ~/.vnc/xstartup
+    echo -e '[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources' >> ~/.vnc/xstartup
+    echo -e 'xsetroot -solid grey' >> ~/.vnc/xstartup
+    echo -e 'vncconfig -iconic &' >> ~/.vnc/xstartup
+    echo -e 'x-terminal-emulator -geometry 80x24+10+10 -ls -title "$VNCDESKTOP Desktop" &' \
+    >> ~/.vnc/xstartup
+    echo -e 'x-window-manager &' >> ~/.vnc/xstartup
+    echo -e 'mate-session &' >> ~/.vnc/xstartup
+
+elif [ "$q" = "2" ] ; then
+
+    echo '#!/bin/bash' > ~/.vnc/xstartup
+    echo 'xrdb $HOME/.Xresources' >> ~/.vnc/xstartup
+    echo 'startxfce4 &' >> ~/.vnc/xstartup
+    
+
+fi
+# ------------------------------------------------------------------------------------------------------- #
+
+
+# CREATE vnc.start FILE # ------------------------------------------------------------------------------- #
 chmod +x ~/.vnc/xstartup
 echo '/usr/bin/vncserver -localhost -depth 24 -geometry 1250x750' > /root/vnc.start
 chmod +x /root/vnc.start
-# ---------------------------------------------------------------------------------------- #
+# ------------------------------------------------------------------------------------------------------- #
 
-sleep 3
+
+# CLEANUP SHELL # --------------------------------------------------------------------------------------- #
 clear
+# ------------------------------------------------------------------------------------------------------- #
 
 
-# Step 04 # --------------------------------------------------------------------------------------------- #
-echo -e "[>] --------------------------------------------------------------------------------- [<]"
-echo -e "\e[92m[>] for start vnc server : bash /root/vnc.start\e[0m"
-echo -e "\e[91m[>] for stop vnc server : bash /root/vnc.kill\e[0m"
-echo -e "\e[93m[>] vnc start at localhost:5901 for more security\e[0m"
-echo -e "\e[93m[>] connect to server using ssh tunnel\e[0m"
-echo -e "\e[92m[>] ssh -L 5901:localhost:5901 root@1.2.3.4\e[0m"
-echo -e "\e[93m[>] open remmina in your client [laptop or pc] and connect to localhost:5901\e[0m"
-echo -e "[>] --------------------------------------------------------------------------------- [<]"
+# CREATE BACKUP FROM motd FILE # ------------------------------------------------------------------------ #
+mv /etc/motd /etc/motd.backup &> /dev/null
+# ------------------------------------------------------------------------------------------------------- #
+
+
+# CREATE /etc/motd FILE # ------------------------------------------------------------------------------- #
+cat << EOF > /etc/motd
+[>] --------------------------------------------------------------------------------- [<]
+[>] for start vnc server : bash /root/vnc.start
+[>] for stop vnc server : bash /root/vnc.kille
+[>] vnc start at localhost:5901 for more security
+[>] connect to server using ssh tunnel
+EOF
+# ------------------------------------------------------------------------------------------------------- #
+
+
+# CREATE /etc/motd FILE # ------------------------------------------------------------------------------- #
+for ip in $(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p') ; do
+    echo -e "[>] ssh -L 5901:localhost:5901 root@$ip" >> /etc/motd
+done
+# ------------------------------------------------------------------------------------------------------- #
+
+
+# CREATE /etc/motd FILE # ------------------------------------------------------------------------------- #
+cat << EOF >> /etc/motd
+[>] open remmina in your client [laptop or pc] and connect to localhost:5901
+[>] --------------------------------------------------------------------------------- [<]
+EOF
+# ------------------------------------------------------------------------------------------------------- #
+
+
+# PRINT MSG TO TERMINAL # ------------------------------------------------------------------------------- #
+echo -en "\e[92m"
+cat /etc/motd
+echo -en "\e[0m"
+# ------------------------------------------------------------------------------------------------------- #
+
+
+# CLEANUP APT CACHE # ----------------------------------------------------------------------------------- #
+apt-get -y clean
 # ------------------------------------------------------------------------------------------------------- #
